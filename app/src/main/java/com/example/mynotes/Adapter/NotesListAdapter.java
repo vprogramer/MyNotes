24d39da77d061353f2cc345cad67f2151a6ed6fc
vprogramer <vlad.bond.1998@mail.ru>
2022-11-18 15:15:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynotes/Adapter/NotesListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.mynotes.Adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynotes.R;
+
+public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
+
+        Context context;
+//        List<Notes> list;
+        @NonNull
+        @Override
+        public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+                return 0;
+        }
+}
+
+class NotesViewHolder extends RecyclerView.ViewHolder {
+
+        CardView notes_container;
+        CheckBox checkBox_note;
+        TextView textView_title, textView_date;
+
+        public NotesViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                notes_container = itemView.findViewById(R.id.notes_container);
+                checkBox_note = itemView.findViewById(R.id.checkBox_note);
+                textView_title = itemView.findViewById(R.id.textView_title);
+                textView_date = itemView.findViewById(R.id.textView_date);
+        }
+}
